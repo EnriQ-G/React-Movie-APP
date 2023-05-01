888d300a391219b61876853bc7335b9ccaf80d43
Enrique Graniel <legraniel.91@gmail.com>
2023-05-01 17:52:42 -0600

    Creación de componentes básicos integración de api y funciones de busqueda

--- React-Movie-App/src/App.jsx
+++ React-Movie-App/src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import MovieCards from './components/MovieCards'
+import SearchBar from './components/SearchBar'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState([])
+  const API_KEY = import.meta.env.MOVIE_DB
+
+  const sendSearch = (search) => {
+      fetch(`https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}&query=${search}`)
+        .then(res => res.json())
+        .then(results => {
+          const { data } = results
+          setMovies(data)
+        }).catch(err => console.log(err))
+    }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React Movie App</h1>
+      <SearchBar className='search-input' onChange={sendSearch}/>
+      <div className='grid-cards'>
+          {
+            movies.map(movie => (
+              <MovieCards
+                title={movie.title}
+                url={movie.poster_path}
+                key={movie.id}
+              />
+            ))
+          }
+        </div>
     </>
   )
 }
---
+++ React-Movie-App/src/components/MovieCards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MovieCards = () => {
+  return (
+    <div className='image-container'>
+      <img src={url} alt={title} className='image-view' />
+      <div></div>
+    </div>
+  )
+}
+
+export default MovieCards
